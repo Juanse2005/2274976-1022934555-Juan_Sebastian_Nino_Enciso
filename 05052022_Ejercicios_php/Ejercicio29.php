b7e564576f66265ec9a95422cd67eba417034eb5
Juanse2005 <101894241+Juanse2005@users.noreply.github.com>
2022-05-19 16:53:32 -0500

    Add files via upload
    Ejercicios de programación en PHP

---
+++ 05052022_Ejercicios_php/Ejercicio29.php
@@ -0,0 +1,22 @@
+<?php
+$servidor="localhost";// 127.0.0.1
+$usuario="root";
+$contrasena="";
+
+try{
+    $conexion=new PDO("mysql:host=$servidor;dbname=album", $usuario, $contrasena);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "Conexion establecida";
+
+    $sql="SELECT * FROM `fotos`";
+
+    $sentencia=$conexion->prepare($sql);
+    $sentencia->execute();
+
+    $resultado=$sentencia->fetchAll();
+
+    //print_r($resultado);
+
+    foreach($resultado as $foto){
+        echo $foto['nombre']."<br/>";
+    }
