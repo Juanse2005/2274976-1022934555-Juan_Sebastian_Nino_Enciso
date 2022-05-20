be5eaf8f185273cf4975557ed6d0cf5e17ddecf8
Juanse2005 <101894241+Juanse2005@users.noreply.github.com>
2022-05-20 16:16:19 -0500

    Delete 05052022_Ejercicios_php directory

--- 05052022_Ejercicios_php/Ejercicio30.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION["usuario"]="develoteca";
-$_SESSION["estatus"]="logueado";
-
-echo "Sesion iniciada".":<br/>";
-
-echo"Usuario: ".$_SESSION["usuario"]." estatus: ".$_SESSION["estatus"];
-?>
